adb243ef9a10925bf1ab60e62b292eca57540a3c
Kimyongdo <giyo1128@gmail.com>
2020-01-23 17:48:19 +0900

    코틀린 제작소

--- app/src/main/java/com/example/makeatimerappuserinterfaceep1/MainActivity.kt
+++ app/src/main/java/com/example/makeatimerappuserinterfaceep1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.makeatimerappuserinterfaceep1
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.health.TimerStat
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
@@ -9,6 +11,16 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    enum class TimerState{
+        Stopped, Paused, Running
+    }
+
+    private lateinit var timer: CountDownTimer
+    private var timerLengthSeconds = 0L //:Long을 참조하지 않아도 가능
+    private var timerState = TimerState.Stopped
+
+    private  var secondReamining = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +31,40 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setIcon(R.drawable.ic_timer)
         supportActionBar?.title="     Timer"
 
+        fab_start.setOnClickListener{v->
+            startTimer() //시작
+            timerState = TimerState.Running //멈춤->진행
+            updateButtons()
+        }
+
+        fab_pause.setOnClickListener{v->
+            timer.cancel() //카운트 취소
+            timerState = TimerState.Paused//중지
+            updateButtons()
+        }
+
+        fab_stop.setOnClickListener{v->
+            timer.cancel()
+            onTimerFinished()
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        initTimer()
+
+        //TODO: remove backgroundTimer, hid notification
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if(timerState==TimerState.Running){//진행중에 다른 화면으로 갔다면
+            timer.cancel()
+            //TODO: start background Timer and show notification
+        }else if(timerState == TimerState.Paused)
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/example/makeatimerappuserinterfaceep1/until/PrefUtil.kt
@@ -0,0 +1,15 @@
+package com.example.makeatimerappuserinterfaceep1.until
+
+import android.content.Context
+
+class PrefUtil {
+    //static같은 역할을 한다는데 그런가요?
+    companion object{
+        fun getTimerLength(context: Context):Int{
+            //placeholder
+            return 1
+        }
+        
+        private  const val
+    }
+}
